623a8db84af781c2aec44c77519dd12e2469976f
YassoKhalid <153525555+YassoKhalid@users.noreply.github.com>
2024-06-14 17:28:48 +0300

    Update Z Algorithm.cpp

--- Z Algorithm.cpp
+++ Z Algorithm.cpp
@@ -1,15 +1,18 @@
-vector<int> z_algo(string &s) {
+vector<int> z_function(string s) {
     int n = s.size();
     vector<int> z(n);
-    z[0] = n;
-    for (int i = 1, L = 1, R = 1; i < n; i++) {
-        int k = i - L;
-        if (z[k] + i >= R) {
-            L = i;
-            R = max(R, i);
-            while (R < n && s[R - L] == s[R]) R++;
-            z[i] = R - L;
-        } else z[i] = z[k];
+    int l = 0, r = 0;
+    for(int i = 1; i < n; i++) {
+        if(i < r) {
+            z[i] = min(r - i, z[i - l]);
+        }
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if(i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
     }
     return z;
 }
