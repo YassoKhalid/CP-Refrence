e99e77242264d7e9c5091d94cb62975d89dacb2c
YassoKhalid <153525555+YassoKhalid@users.noreply.github.com>
2024-06-21 05:00:49 +0300

    Update Matrix Power.cpp

--- Matrix Power.cpp
+++ Matrix Power.cpp
@@ -1,29 +1,30 @@
+template<typename T>
 struct Mat {
     int n, m;
-    vector<vector<int> > mat;
+    vector<vector<T> > mat;
 
     Mat() {}
 
     Mat(int _n, int _m) {
         n = _n;
         m = _m;
-        mat = vector<vector<int>>(n, vector<int>(m));
+        mat = vector<vector<T>>(n, vector<T>(m));
     }
 
     Mat(int _n) {
         n = _n;
         m = _n;
-        mat = vector<vector<int>>(n, vector<int>(m));
+        mat = vector<vector<T>>(n, vector<T>(m));
     }
 
-    Mat(vector<vector<int> > v) {
+    Mat(vector<vector<T> > v) {
         mat = v;
         n = (int) v.size();
         m = (int) v[0].size();
     }
 
     void id(int _n) {
-        mat = vector<vector<int>>(_n, vector<int>(_n));
+        mat = vector<vector<T>>(_n, vector<T>(_n));
         for (int i = 0; i < _n; i++)
             mat[i][i] = 1;
     }
@@ -34,7 +35,7 @@ struct Mat {
             for (int j = 0; j < b.m; j++)
                 for (int k = 0; k < b.m; k++) {
                     ans.mat[i][j] += mat[i][k] * b.mat[k][j];
-                    // ans.mat[i][j] %= mod;
+//                    ans.mat[i][j] %= mod;
                 }
         return ans;
     }
@@ -44,17 +45,6 @@ struct Mat {
         for (int i = 0; i < b.n; i++)
             for (int j = 0; j < b.m; j++)
                 ans.mat[i][j] = mat[i][j] + b.mat[i][j];
-        return ans;
-    }
-
-    Mat exp(Mat a, int b) {
-        Mat ans = a;
-        ans.id(a.n);
-        while (b) {
-            if (b & 1) ans = ans * a;
-            a = a * a;
-            b >>= 1;
-        }
         return ans;
     }
 
